a96db8f8c754bb3c533215eea8073cde9a90c903
Afzal Aghariya <afzal.mobilefirst@gmail.com>
2023-10-07 10:36:27 +0530

    Added sdwebImage

--- Sources/MyLibrary/ViewController/ContentVC.swift
+++ Sources/MyLibrary/ViewController/ContentVC.swift
@@ -6,12 +6,14 @@
 //
 
 import UIKit
+import SDWebImage
 
 class ContentVC: UIViewController {
 
+    @IBOutlet weak var imgPoster: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
+        imgPoster.sd_setImage(with: URL(string: "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1200"))
     }
 }
